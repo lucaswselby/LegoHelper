c342c5f18bffe96411a292194edac43d16763f94
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2024-04-30 11:42:32 -0700

    Added set 7314

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ copy details from https://www.bricklink.com/v2/main.page
 */
 let input = ``;
 let noInput = input ? false : true;
-let customPartNumber = 543;
+let customPartNumber = 545;
 if (input) {
   // removes unnecessary information
   input = input.replaceAll("  +	", "");
